9a3c475181311f11f171401cdefd3f535b8bf9fb
peachplum <58638328+peachplum@users.noreply.github.com>
2021-05-27 13:47:10 -0400

    created TriviaActivity (wip)

--- app/src/main/java/com/example/braintraining/ui/play/PlayActivity.java
+++ app/src/main/java/com/example/braintraining/ui/play/PlayActivity.java
@@ -13,6 +13,7 @@ import com.example.braintraining.R;
 public class PlayActivity extends AppCompatActivity {
 
     Button BackButton;
+    Button TriviaButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,12 +21,22 @@ public class PlayActivity extends AppCompatActivity {
         setContentView(R.layout.activity_play);
 
         BackButton = (Button)findViewById(R.id.backButton);
+        TriviaButton = (Button)findViewById(R.id.triviaButton);
 
         BackButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(PlayActivity.this,
                         MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        TriviaButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(PlayActivity.this,
+                        TriviaActivity.class);
                 startActivity(intent);
             }
         });
---
+++ app/src/main/java/com/example/braintraining/ui/play/TriviaActivity.java
@@ -0,0 +1,16 @@
+package com.example.braintraining.ui.play;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.braintraining.R;
+
+public class TriviaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trivia);
+    }
+}
