768d56035e8486c0ac13fd47aa6a2045f7d67cca
peachplum <58638328+peachplum@users.noreply.github.com>
2021-06-08 13:06:06 -0400

    base trivia game completed

--- app/src/main/java/com/example/braintraining/ui/play/TriviaActivity.java
+++ app/src/main/java/com/example/braintraining/ui/play/TriviaActivity.java
@@ -4,16 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.os.Handler;
 
 import com.example.braintraining.MainActivity;
 import com.example.braintraining.R;
-import com.example.braintraining.ui.settings.SettingsActivity;
 
 import java.util.*;
 
@@ -25,7 +26,9 @@ public class TriviaActivity extends AppCompatActivity {
         setContentView(R.layout.activity_trivia);
 
         Button backButton;
+        Button enterButton;
         Random random = new Random();
+        final Handler handler = new Handler();
 
         String[] triviaQuestions = new String[5];
         triviaQuestions[0] = "question 1";
@@ -34,13 +37,53 @@ public class TriviaActivity extends AppCompatActivity {
         triviaQuestions[3] = "question 4";
         triviaQuestions[4] = "question 5";
 
+        String[] triviaAnswers = new String[5];
+        triviaAnswers[0] = "answer 1";
+        triviaAnswers[1] = "answer 2";
+        triviaAnswers[2] = "answer 3";
+        triviaAnswers[3] = "answer 4";
+        triviaAnswers[4] = "answer 5";
+
+        final boolean[] clickedButton = {false};
+
         int triviaQuestionNumber = random.nextInt(5);
 
         TextView textTrivia = (TextView)findViewById(R.id.textTrivia);
         textTrivia.setText(triviaQuestions[triviaQuestionNumber]);
 
         EditText triviaInput = (EditText) findViewById(R.id.triviaEditText);
-        String triviaAnswer = triviaInput.getText().toString();
+
+        TextView responseText = (TextView)findViewById(R.id.responseText);
+
+        enterButton = (Button)findViewById(R.id.enterButton);
+
+        String response = getString(R.string.text_incorrect) + " " + triviaAnswers[triviaQuestionNumber];
+
+        enterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String triviaUserAnswer = triviaInput.getText().toString();
+                if (triviaUserAnswer.equals(triviaAnswers[triviaQuestionNumber])) {
+                    responseText.setText(R.string.text_correct);
+                    handler.postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            finish();
+                            startActivity(getIntent());
+                        }
+                    }, 5000);
+                } else {
+                    responseText.setText(response);
+                    handler.postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            finish();
+                            startActivity(getIntent());
+                        }
+                    }, 5000);
+                }
+            }
+        });
 
         backButton = (Button)findViewById(R.id.backButton4);
         final MediaPlayer mp = MediaPlayer.create(this, R.raw.byebye);
